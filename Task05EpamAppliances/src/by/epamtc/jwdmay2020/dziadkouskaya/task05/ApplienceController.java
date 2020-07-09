7d792c28363dec6934473585fd559216ca6bda36
Yana Dziadkouskaya <yana.dziadkouskaya@gmail.com>
2020-07-09 22:02:00 +0300

    Commint 5: changed ApplienceController

--- Task05EpamAppliances/src/by/epamtc/jwdmay2020/dziadkouskaya/task05/ApplienceController.java
+++ Task05EpamAppliances/src/by/epamtc/jwdmay2020/dziadkouskaya/task05/ApplienceController.java
@@ -15,24 +15,28 @@ import by.epamtc.jwdmay2020.dziadkouskaya.task05.logic.RequestCreation;
 
 public class ApplienceController {
 	public static void main(String[] args) {
-				
-		List<String> requstlist = new ArrayList<>(Arrays.asList("Laptop", "MEMORY_ROM", "8000", "CPU" , "2.2"));
-		
+
+		List<String> requstlist = new ArrayList<>(Arrays.asList("Laptop", "MEMORY_ROM", "8000", "CPU", "2.2"));
+
 		UserRequest request = RequestCreation.createRequesFromList(requstlist);
-			
+
 		String fileName = "d:\\Java\\epam\\tasks\\appliances_db.txt";
-		
+
 		InfoSourse source = new TextFileInfoSource(fileName);
-		
+
 		Catallogue catallogue = CatalogCreation.createCatalog(source, request);
-		
+
 		List<Applience> applienceList = ApplienceListCreation.createListOfRequiredApplience(catallogue);
-		
-		for (Applience applience : applienceList) {
-			System.out.println(applience);
+
+		if (applienceList.size() == 0) {
+			System.out.println("No results!!!");
+		} else {
+
+			for (Applience applience : applienceList) {
+				System.out.println(applience);
+			}
 		}
-		
-		
+
 	}
 
 }
